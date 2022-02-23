58000216f43b93f1629f2eae109ba8f93153f901
hannahchobson <71147573+hannahchobson@users.noreply.github.com>
2022-02-22 19:57:23 -0500

    Add files via upload

--- chess.js
+++ chess.js
@@ -7,3 +7,20 @@ var board2 = Chessboard('board2', {
 
 $('#startBtn').on('click', board2.start)
 $('#clearBtn').on('click', board2.clear)
+
+
+//create the modal over screen for player 1
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementByClassName("close")[0];
+btn.onclick = function(){
+  modal.style.display = "block";
+}
+span.onclick = function(){
+  modal.style.display = "none";
+}
+window.onclick = function(event){
+  if(event.target == modal){
+    modal.style.display = "none";
+  }
+}
